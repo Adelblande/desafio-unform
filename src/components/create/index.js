9c88df6213c2d61db6469026f87aec50331b87cc
Adelblande <adelblande@gmail.com>
2019-05-17 01:34:42 -0300

    iniciando com unform

--- src/components/create/index.js
+++ src/components/create/index.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from 'react'
+import { Form, Input } from "@rocketseat/unform"
 
 // import { Container } from './styles';
 
-const Create = () => <h1>Create</h1>;
-
+const Create = () => {
+    function handleSubmit(data) {
+        console.log(data)
+    }
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Input name="nome" />
+            <Input name="telefone" />
+            <Input name="email" />
+            <button type="submit">Enviar</button>
+        </Form>
+    )
+}
 export default Create;

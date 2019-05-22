12fef6556699807cc1bd27f8b27f65aabb9f58bf
Adelblande <adelblande@gmail.com>
2019-05-22 08:39:07 -0300

    Criando o Read

--- src/components/read/index.js
+++ src/components/read/index.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 // import { Container } from './styles';
 
-const Read = () => <h1>Read</h1>;
+import api from '../../services/api';
+
+const Read = () => {
+  const [contacts, setContacts] = useState()
+
+  useEffect(() => {
+    async function getContacts() {
+      const { data } = await api.get('/contatos')
+      setContacts([...data])
+    }
+    getContacts()
+  }, [contacts])
+
+  return (
+    <Container>
+      {
+        contacts && contacts.map(contact => (
+          <Row key={contact._id} className="justify-content-xs-center">
+            <Col sm={3}>{ contact.nome }</Col>
+            <Col sm={3}>{ contact.telefone }</Col>
+            <Col sm={3}>{ contact.email }</Col>
+          </Row>
+        ))
+      }
+    </Container>
+  
+  )
+};
 
 export default Read;
